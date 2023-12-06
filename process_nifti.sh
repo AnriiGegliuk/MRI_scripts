1f3577ffa30f50138d91fdde9be3341ff588c37a
Andrii Gegliuk <120349975+AnriiGegliuk@users.noreply.github.com>
2023-12-06 13:49:22 +0900

    .nii.src.gz extention amended to open this file in dsi_studio

--- process_nifti.sh
+++ process_nifti.sh
@@ -13,7 +13,7 @@ for nii_file in "${dir}"/*.nii; do
     # need to extract name for each file to use them latter durring recontruction
     base_name=$(basename "${nii_file}" .nii)
     # output with the same name as input
-    output_file="${output_dir}/${base_name}.src.gz"
+    output_file="${output_dir}/${base_name}.nii.src.gz"
     # final dsi studio command
     dsi_studio --action=src --source="${nii_file}" --b_table="${dir}/b_table_new.txt" --output="${output_file}"
 done
