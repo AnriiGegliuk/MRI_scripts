080928daef1619284e18b6d659bccc6409078f8b
AnriiGegliuk <andrii.gegliuk@gmail.com>
2023-12-25 14:57:37 +0900

    comments modified

--- process_nifti.sh
+++ process_nifti.sh
@@ -5,15 +5,15 @@ dir=$(pwd)
 # directory to save the output SRC files
 output_dir="${dir}/src_folder"
 
-# making new output directory if it doesn't exist
+# making new output directory if doesn't exist
 mkdir -p "${output_dir}"
 
-# Processing each NIfTI file
+# processing each NIfTI file in cwd
 for nii_file in "${dir}"/*.nii; do
     # need to extract name for each file to use them latter durring recontruction
     base_name=$(basename "${nii_file}" .nii)
     # output with the same name as input
     output_file="${output_dir}/${base_name}.nii.src.gz"
-    # final dsi studio command
+    # final dsi studio command that will preprocess all nii to src
     dsi_studio --action=src --source="${nii_file}" --b_table="${dir}/b_table_new.txt" --output="${output_file}"
 done
